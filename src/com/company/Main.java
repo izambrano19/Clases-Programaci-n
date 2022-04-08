d8f8ddf5ac3c87d67c76f8f251042390d3acf39f
Ismael Zambrano Rodríguez <91273451+izambrano19@users.noreply.github.com>
2022-04-08 19:33:21 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void imprimirMatriz(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             for(int j = 0; j < matriz[0].length; j++) {
-                System.out.print(matriz[i][j]+" ");
+                System.out.print(matriz[i][j]+ " ");
             }
             System.out.println("\n");
         }
